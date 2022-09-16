7fa9e914dc17688c740a8f68640aa2ada6440901
Mehdi Mehdizadeh <mehdizadeh.ws@gmail.com>
2022-09-16 08:57:10 +0430

    First commit

--- ToastrPlugin.php
+++ ToastrPlugin.php
@@ -17,7 +17,7 @@ class ToastrPlugin extends Plugin
     public function getScripts()
     {
         return array(
-            'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
+            'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js111',
             'https://cdn.jsdelivr.net/npm/@flasher/flasher-toastr@1.1.1/dist/flasher-toastr.min.js',
         );
     }
